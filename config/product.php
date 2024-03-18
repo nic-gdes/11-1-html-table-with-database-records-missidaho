266bbb7e407ebf05239aa637ab29065a8fa8babd
Madeleine <mgleahy@nic.edu>
2024-03-18 14:22:31 -0700

    working on ddev

---
+++ config/product.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Create connection to DB with mysqli_connect
+// $conn = mysqli_connect ("hostname, username, password, database name")
+$conn = mysqli_connect("db:3306", "root", "root", "product");
+
+//verify connection with mysqli
+if(mysqli_connect_errno ()) {
+    echo "Database error: ". mysqli_connect_errno ();
+    exit();
+}
+
+?>

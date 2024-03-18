da35330bedb769b5f7e13bb94c14bc191ff2fd22
Madeleine <mgleahy@nic.edu>
2024-03-18 15:50:22 -0700

    finished

--- config/products.php
+++ config/products.php
@@ -2,7 +2,7 @@
 
 // Create connection to DB with mysqli_connect
 // $conn = mysqli_connect ("hostname, username, password, database name")
-$conn = mysqli_connect("db:3306", "root", "root", "product");
+$conn = mysqli_connect("db:3306", "root", "root", "products");
 
 //verify connection with mysqli
 if(mysqli_connect_errno ()) {
--- index.php
+++ index.php
@@ -1,3 +1,44 @@
-<?php 
-include ('./config/product.php');
+<?php
+
+include('./config/products.php');
+$sql = "SELECT id, name, price FROM items1";
+
+//query the db and add the result to a php array
+$result = mysqli_query($conn, $sql);
+$items = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//  print_r($items);
+//free result from memory and close SQL connection
+mysqli_free_result($result);
+mysqli_close($conn);
+
 ?>
+<html>
+    <link rel="stylesheet" href="./assets/style.css">
+    <div class="wrapper">
+<table>
+    <tr>
+        <th>ITEMS</th>
+        <th>PRICE</th>
+
+    </tr>
+    <tr>
+        <td>
+            <?php foreach ($items as $item) : ?>
+                <p><?php echo $item["name"] ?></p>
+            <?php endforeach ?>
+        </td>
+        <td>
+        <?php foreach ($items as $item) : ?>
+                <p><?php echo $item["price"] ?></p>
+            <?php endforeach ?>
+        </td>
+    </tr>
+
+</table>
+</div>
+
+<!-- <?php foreach ($items as $item) : ?>
+    <h2><?php echo $item["name"] ?></h2>
+<?php endforeach ?> -->
+
+</html>
